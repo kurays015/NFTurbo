039793458882946ac0c77c1b6b6f06b21a29aa91
Kurays <christnarvarte15@gmail.com>
2025-07-08 01:49:51 +0800

    fix

--- raffle-tool/components/NFTRaffleRace.tsx
+++ raffle-tool/components/NFTRaffleRace.tsx
@@ -401,7 +401,7 @@ export default function NFTRaffleRace() {
               />
             )}
             {/* NFT Display */}
-            {!userAddress && (
+            {userAddress && (
               <NFTDisplay
                 selectedNFT={selectedNFT}
                 isApprovalTxSuccess={isApprovalTxSuccess}
