764bee01fbb43a0760b0705f1264a1bea8eeb30b
Christzxz <126612073+kurays015@users.noreply.github.com>
2025-07-14 01:05:17 +0800

    Update useMagicEdenNFTsData.ts

--- raffle-tool/hooks/useMagicEdenNFTsData.ts
+++ raffle-tool/hooks/useMagicEdenNFTsData.ts
@@ -2,7 +2,7 @@ import { CollectionResponse } from "@/types/collection";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const fetchUserCollections = async (userAddress: `0x${string}`): Promise<CollectionResponse> => {
+const fetchUserCollections = async (userAddress: `0x${string}` | undefined): Promise<CollectionResponse> => {
   const response = await axios.get<CollectionResponse>(
     `https://api-mainnet.magiceden.dev/v3/rtp/monad-testnet/users/${userAddress}/collections/v3?includeTopBid=false&includeLiquidCount=false&offset=0&limit=100`
   );
