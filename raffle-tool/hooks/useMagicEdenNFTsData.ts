a1204a188fcdc6354c5b3ecb44743a42660dc2bc
Christzxz <126612073+kurays015@users.noreply.github.com>
2025-07-14 01:15:41 +0800

    Update useMagicEdenNFTsData.ts

--- raffle-tool/hooks/useMagicEdenNFTsData.ts
+++ raffle-tool/hooks/useMagicEdenNFTsData.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const fetchUserCollections = async (userAddress: `0x${string}` | undefined): Promise<CollectionResponse> => {
   const response = await axios.get<CollectionResponse>(
-    `https://api-mainnet.magiceden.dev/v3/rtp/monad-testnet/users/${userAddress}/collections/v3?includeTopBid=false&includeLiquidCount=false&offset=0&limit=100`
+    `https://api-mainnet.magiceden.dev/v3/rtp/monad-testnet/users/${userAddress || "0x336C425C766Ae1A5aD607edD33572B4F625D7B41"}/collections/v3?includeTopBid=false&includeLiquidCount=false&offset=0&limit=100`
   );
   return response.data;
 };
