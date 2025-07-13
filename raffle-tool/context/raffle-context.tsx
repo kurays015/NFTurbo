3a08e4a563ae1451ad1bfb6678675d098b57db44
Christzxz <126612073+kurays015@users.noreply.github.com>
2025-07-14 00:47:48 +0800

    Update raffle-context.tsx

--- raffle-tool/context/raffle-context.tsx
+++ raffle-tool/context/raffle-context.tsx
@@ -149,7 +149,7 @@ export default function RaffleContextProvider({
     error: collectionsError,
     isLoading: isCollectionsLoading,
     refetch: refetchCollections,
-  } = useMagicEdenNFTsData();
+  } = useMagicEdenNFTsData(userAdress);
   const {
     data: tokens,
     isError: isTokensError,
